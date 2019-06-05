cd88007f6320617a659a6173a1dddbec8e9b1a20
matiqo1234r <madsries321@gmail.com>
2019-06-05 21:53:49 +0200

    add a sprite and background

---
+++ sprite.py
@@ -0,0 +1,33 @@
+
+class Duszek():
+
+    def rysuj(self):
+
+
+
+    def ustawpozycję(self):
+
+
+
+
+    def pokaż(self):
+
+
+
+
+    def ukruj(self):
+
+
+
+
+    def zniszcz(self):
+
+
+
+
+    def pozycja(self):
+
+
+
+
+    def rozmiar(self):
